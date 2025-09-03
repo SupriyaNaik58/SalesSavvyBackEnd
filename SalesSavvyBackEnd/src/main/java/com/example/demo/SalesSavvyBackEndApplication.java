b84f8a09d6ca1296c026943c767c247582495509
naik5 <naik5@LAPTOP-1HV3F22V>
2025-09-03 16:57:42 +0530

    First commit

---
+++ SalesSavvyBackEnd/src/main/java/com/example/demo/SalesSavvyBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesSavvyBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesSavvyBackEndApplication.class, args);
+	}
+
+}
